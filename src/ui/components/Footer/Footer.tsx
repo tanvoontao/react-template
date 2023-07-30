9f2ab1311e5aa720942f031cf0509bf9cdd8aae7
tanvoontao <tanvoontao0908@gmail.com>
2023-07-30 15:16:02 +0800

    .

---
+++ src/ui/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="fixed inset-x-0 bottom-0 bg-blue-600 text-white text-center py-4">
+      Footer Content
+    </footer>
+  );
+};
+
+export default Footer;
